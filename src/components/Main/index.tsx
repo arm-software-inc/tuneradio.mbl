cd99d6afb3d887bfc7732b8a6f0f5373aeceeb01
murillo.nahas <murillonahas@hotmail.com>
2023-10-02 12:28:58 -0300

    feat: changing folders structure

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,10 +1,11 @@
+import Header from "../Header";
 import { Container, Text } from "./styles";
 
 export function Main() {
   return (
     <>
       <Container>
-        <Text>Starting Radiao</Text>
+        <Header />
       </Container>
     </>
   );
