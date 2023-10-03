fb03a8c7dbd769a48b442c037895dfb99fe8b2fb
murillo.nahas <murillonahas@hotmail.com>
2023-10-02 21:58:54 -0300

    feat: starting sign in page

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,17 +1,73 @@
-import { Text, Button, Image } from "react-native";
-import { ContainerView } from "../../components/Container/styles";
+import { Text } from "react-native";
 
-import { Logo } from "./styles";
+import {
+  Logo,
+  ContainerView,
+  StyledTextInput,
+  StyledLabel,
+  StyledDiv,
+  StyledButton,
+  StyledButtonText,
+} from "./styles";
+
+import { useForm, Controller } from "react-hook-form";
 
 export function SignIn({ navigation }: any) {
+  const {
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  function onSubmit(data: any) {
+    console.log(data);
+    navigation.navigate("SignUp");
+  }
+
   return (
     <ContainerView>
       <Logo source={require("../../assets/icons/Icon.png")} />
-      <Text>Radião</Text>
-      {/* <Button
-        title="Ir para Sign Up"
-        onPress={() => navigation.navigate("SignUp")}
-      /> */}
+
+      <Controller
+        control={control}
+        render={({ field: { onChange, onBlur, value } }) => (
+          <StyledDiv>
+            <StyledLabel>Email</StyledLabel>
+            <StyledTextInput
+              onChangeText={onChange}
+              onBlur={onBlur}
+              value={value}
+              placeholder="johndoe@mail.com"
+            />
+          </StyledDiv>
+        )}
+        name="email"
+        rules={{ required: "E-mail is required." }}
+      />
+      {errors.email && <Text>Invalid e-mail.</Text>}
+
+      <Controller
+        control={control}
+        render={({ field: { onChange, onBlur, value } }) => (
+          <StyledDiv>
+            <StyledLabel>Password</StyledLabel>
+            <StyledTextInput
+              onChangeText={onChange}
+              onBlur={onBlur}
+              value={value}
+              placeholder="Password"
+              secureTextEntry
+            />
+          </StyledDiv>
+        )}
+        name="password"
+        rules={{ required: "Password is required." }}
+      />
+      {errors.password && <Text>Invalid password.</Text>}
+
+      <StyledButton onPress={handleSubmit(onSubmit)} title="Sign In">
+        <StyledButtonText>Sign In</StyledButtonText>
+      </StyledButton>
     </ContainerView>
   );
 }
--- src/pages/SignIn/styles.ts
+++ src/pages/SignIn/styles.ts
@@ -1,7 +1,63 @@
+import {
+  Platform,
+  StatusBar,
+  View,
+  TouchableOpacity,
+  Text,
+} from "react-native";
 import styled from "styled-components/native";
 
+const isAndroid = Platform.OS === "android";
+
+export const ContainerView = styled.SafeAreaView`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin-top: ${isAndroid ? `${StatusBar.currentHeight}px` : "0"};
+`;
+
+export const StyledDiv = styled(View)``;
+
 export const Logo = styled.Image`
-  margin-top: 24px;
+  margin: 42px 0px;
   width: 161.47px;
   height: 135.03px;
+`;
+
+export const StyledLabel = styled.Text`
+  color: #303030;
+  font-weight: bold;
+  font-size: 18px;
+  margin-top: 42px;
+`;
+
+export const StyledTextInput = styled.TextInput`
+  margin-top: 8px;
+  width: 300px;
+  height: 50px;
+  border-radius: 5px;
+  padding: 8px;
+  border: 1px solid #303030;
+  border-bottom: 4px solid #303030;
+  border-right: 4px solid #303030;
+  outline: 0;
+`;
+
+export const StyledButton = styled(TouchableOpacity)`
+  margin-top: 42px;
+  width: 300px;
+  height: 50px;
+  background-color: #ffec74;
+  border: 1px solid #303030;
+  border-radius: 5px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const StyledButtonText = styled(Text)`
+  color: #303030;
+  font-size: 16px;
+  font-weight: bold;
+  text-align: center;
 `;
