d60e294558daa7edc255590f3f3fce53e6d9aa59
murillo.nahas <murillonahas@hotmail.com>
2023-10-03 11:12:54 -0300

    feat: finishing sign in page MVP

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -8,6 +8,16 @@ import {
   StyledDiv,
   StyledButton,
   StyledButtonText,
+  StyledCheckbox,
+  StyledCheckboxDiv,
+  StyledCheckboxText,
+  StyledButtonGoogle,
+  StyledTextGoogle,
+  StyledTextAppleId,
+  StyledButtonAppleId,
+  Divisor,
+  StyledTextAction,
+  StyledDivAction,
 } from "./styles";
 
 import { useForm, Controller } from "react-hook-form";
@@ -65,9 +75,32 @@ export function SignIn({ navigation }: any) {
       />
       {errors.password && <Text>Invalid password.</Text>}
 
+      <StyledCheckboxDiv>
+        <StyledCheckbox />
+        <StyledCheckboxText>Remember me</StyledCheckboxText>
+      </StyledCheckboxDiv>
+
       <StyledButton onPress={handleSubmit(onSubmit)} title="Sign In">
         <StyledButtonText>Sign In</StyledButtonText>
       </StyledButton>
+
+      <StyledButtonGoogle onPress={handleSubmit(onSubmit)} title="Sign In">
+        <StyledTextGoogle>Sign In with Google</StyledTextGoogle>
+      </StyledButtonGoogle>
+
+      <StyledButtonAppleId onPress={handleSubmit(onSubmit)} title="Sign In">
+        <StyledTextAppleId>Sign with Apple Id</StyledTextAppleId>
+      </StyledButtonAppleId>
+
+      <Divisor />
+
+      <StyledDivAction>
+        <StyledTextAction>Don't have an account? Sign Up</StyledTextAction>
+      </StyledDivAction>
+
+      <StyledDivAction>
+        <StyledTextAction>Forgotten your password?</StyledTextAction>
+      </StyledDivAction>
     </ContainerView>
   );
 }
--- src/pages/SignIn/styles.ts
+++ src/pages/SignIn/styles.ts
@@ -12,14 +12,17 @@ const isAndroid = Platform.OS === "android";
 export const ContainerView = styled.SafeAreaView`
   display: flex;
   align-items: center;
-  justify-content: center;
   margin-top: ${isAndroid ? `${StatusBar.currentHeight}px` : "0"};
+  background-color: #f0edd7;
+  height: 100%;
 `;
 
-export const StyledDiv = styled(View)``;
+export const StyledDiv = styled(View)`
+  margin-top: 28px;
+`;
 
 export const Logo = styled.Image`
-  margin: 42px 0px;
+  margin: 16px 0px;
   width: 161.47px;
   height: 135.03px;
 `;
@@ -28,7 +31,6 @@ export const StyledLabel = styled.Text`
   color: #303030;
   font-weight: bold;
   font-size: 18px;
-  margin-top: 42px;
 `;
 
 export const StyledTextInput = styled.TextInput`
@@ -41,10 +43,11 @@ export const StyledTextInput = styled.TextInput`
   border-bottom: 4px solid #303030;
   border-right: 4px solid #303030;
   outline: 0;
+  background-color: #fff;
 `;
 
 export const StyledButton = styled(TouchableOpacity)`
-  margin-top: 42px;
+  margin-top: 24px;
   width: 300px;
   height: 50px;
   background-color: #ffec74;
@@ -60,4 +63,84 @@ export const StyledButtonText = styled(Text)`
   font-size: 16px;
   font-weight: bold;
   text-align: center;
+`;
+
+export const StyledCheckboxDiv = styled(View)`
+  margin-top: 24px;
+  width: 300px;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+`;
+
+export const Divisor = styled(View)`
+  margin-top: 16px;
+  width: 300px;
+  height: 3px;
+  background-color: #303030;
+`;
+
+export const StyledCheckbox = styled.TouchableOpacity`
+  width: 24px;
+  height: 24px;
+  border: 2px solid #303030;
+  border-radius: 5px;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const StyledCheckboxText = styled.Text`
+  color: #303030;
+  font-weight: bold;
+  margin-left: 8px;
+`;
+
+export const StyledButtonGoogle = styled(TouchableOpacity)`
+  margin-top: 16px;
+  width: 300px;
+  height: 50px;
+  background-color: #fff;
+  border: 1px solid #303030;
+  border-radius: 5px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const StyledTextGoogle = styled(Text)`
+  color: #303030;
+  font-size: 16px;
+  font-weight: bold;
+  text-align: center;
+`;
+
+export const StyledButtonAppleId = styled(TouchableOpacity)`
+  margin-top: 16px;
+  width: 300px;
+  height: 50px;
+  background-color: #000;
+  border: 1px solid #303030;
+  border-radius: 5px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const StyledTextAppleId = styled(Text)`
+  color: #fff;
+  font-size: 16px;
+  font-weight: bold;
+  text-align: center;
+`;
+
+export const StyledDivAction = styled(View)`
+  width: 300px;
+  margin-top: 28px;
+`;
+
+export const StyledTextAction = styled(Text)`
+  color: #303030;
+  font-size: 16px;
+  font-weight: bold;
+  text-decoration: underline;
 `;
