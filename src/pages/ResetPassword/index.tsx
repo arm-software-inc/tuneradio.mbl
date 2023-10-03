19b28d0f6233e494b413ed54cf905e70ffa89545
murillo.nahas <murillonahas@hotmail.com>
2023-10-02 21:59:15 -0300

    feat: reset password

--- src/pages/ResetPassword/index.tsx
+++ src/pages/ResetPassword/index.tsx
@@ -1,12 +1,11 @@
 import { Text } from "../../components/Text/Text";
-import { ContainerView } from "../../components/Container/styles";
 
 export function ResetPassword() {
   return (
-    <ContainerView>
+    <>
       <Text size={22} weight="700">
         Reset Password
       </Text>
-    </ContainerView>
+    </>
   );
 }
