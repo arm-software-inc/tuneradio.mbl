768aab3ab5796e22fca1a50baf3ba423e7934358
murillo.nahas <murillonahas@hotmail.com>
2023-10-02 21:59:24 -0300

    feat: sign up page

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -1,12 +1,11 @@
 import { Text } from "../../components/Text/Text";
-import { ContainerView } from "../../components/Container/styles";
 
 export function SignUp() {
   return (
-    <ContainerView>
+    <>
       <Text size={22} weight="700">
         Sign Up Page
       </Text>
-    </ContainerView>
+    </>
   );
 }
