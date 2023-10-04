8fdbb4d73d47c7ddc44d82de3dbf3df6fcb9f8ce
murillo.nahas <murillonahas@hotmail.com>
2023-10-04 18:28:14 -0300

    feat: finishing signIn UI

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,87 +1,101 @@
-import { Text } from "react-native";
+import { useForm, Controller } from "react-hook-form";
 
 import {
   Logo,
   ContainerView,
+  StyledDiv,
+  Title,
+  StyledTextInputWrapper,
   StyledTextInput,
   StyledLabel,
-  StyledDiv,
-  StyledCheckbox,
-  StyledCheckboxDiv,
-  StyledCheckboxText,
-  StyledTextAction,
-  StyledDivAction,
 } from "./styles";
-
-import { useForm, Controller } from "react-hook-form";
 
 import { Button } from "../../components/Button";
 import { Divisor } from "../../components/Divisor";
-import { Checkbox } from "../../components/Forms/Checkbox";
+import { Link } from "../../components/Link";
+import { ErrorLabel } from "../../components/Forms/ErrorLabel";
 
 export function SignIn({ navigation }: any) {
+  const logoGoogle = require("../../assets/icons/GoogleIcon.png");
+
+  const logoApple = require("../../assets/icons/AppleIcon.png");
+
   const {
     control,
+    handleSubmit,
     formState: { errors },
   } = useForm();
 
   function onSubmit() {
+    navigation.navigate("Home");
+  }
+
+  function onSignUp() {
     navigation.navigate("SignUp");
+  }
+
+  function onResetPassword() {
+    navigation.navigate("ResetPassword");
   }
 
   return (
     <ContainerView>
       <Logo source={require("../../assets/icons/Icon.png")} />
 
+      <Title>Radião</Title>
+
       <Controller
         control={control}
         render={({ field: { onChange, onBlur, value } }) => (
           <StyledDiv>
-            <StyledLabel>Email</StyledLabel>
-            <StyledTextInput
-              onChangeText={onChange}
-              onBlur={onBlur}
-              value={value}
-              placeholder="johndoe@mail.com"
-            />
+            <StyledLabel>E-mail</StyledLabel>
+            <StyledTextInputWrapper>
+              <StyledTextInput
+                onChangeText={onChange}
+                onBlur={onBlur}
+                value={value}
+                placeholder="johndoe@mail.com"
+              />
+            </StyledTextInputWrapper>
           </StyledDiv>
         )}
         name="email"
         rules={{ required: "E-mail is required." }}
       />
-      {errors.email && <Text>Invalid e-mail.</Text>}
+      {errors.email && <ErrorLabel label="Invalid e-mail." />}
 
       <Controller
         control={control}
         render={({ field: { onChange, onBlur, value } }) => (
           <StyledDiv>
             <StyledLabel>Password</StyledLabel>
-            <StyledTextInput
-              onChangeText={onChange}
-              onBlur={onBlur}
-              value={value}
-              placeholder="Password"
-              secureTextEntry
-            />
+            <StyledTextInputWrapper>
+              <StyledTextInput
+                onChangeText={onChange}
+                onBlur={onBlur}
+                value={value}
+                placeholder="Password"
+                secureTextEntry
+              />
+            </StyledTextInputWrapper>
           </StyledDiv>
         )}
         name="password"
         rules={{ required: "Password is required." }}
       />
-      {errors.password && <Text>Invalid password.</Text>}
-
-      <Checkbox text="Remember me" />
+      {errors.password && <ErrorLabel label="Invalid password." />}
 
       <Button
         backgroundColor="#ffec74"
         label="Sign In"
         color="#303030"
-        handleSubmit={onSubmit}
+        handleSubmit={handleSubmit(onSubmit)}
       />
 
       <Button
         backgroundColor="#fff"
         label="Sign In with Google"
+        logoURL={logoGoogle}
         color="#303030"
         handleSubmit={onSubmit}
       />
@@ -89,19 +103,15 @@ export function SignIn({ navigation }: any) {
       <Button
         backgroundColor="#000"
         label="Sign In with Apple Id"
+        logoURL={logoApple}
         color="#fff"
         handleSubmit={onSubmit}
       />
 
       <Divisor backgroundColor="#303030" />
 
-      <StyledDivAction>
-        <StyledTextAction>Don't have an account? Sign Up</StyledTextAction>
-      </StyledDivAction>
-
-      <StyledDivAction>
-        <StyledTextAction>Forgotten your password?</StyledTextAction>
-      </StyledDivAction>
+      <Link label="Don't have an account? Sign Up" onNavigate={onSignUp} />
+      <Link label="Forgotten your password?" onNavigate={onResetPassword} />
     </ContainerView>
   );
 }
--- src/pages/SignIn/styles.ts
+++ src/pages/SignIn/styles.ts
@@ -27,22 +27,32 @@ export const Logo = styled.Image`
   height: 135.03px;
 `;
 
+export const Title = styled.Text`
+  color: #303030;
+  font-weight: bold;
+  font-size: 38px;
+`;
+
 export const StyledLabel = styled.Text`
   color: #303030;
   font-weight: bold;
   font-size: 18px;
 `;
 
+export const StyledTextInputWrapper = styled.View`
+  width: 100%;
+  border-radius: 25px;
+  border-bottom-width: 4px;
+  border-color: #a466ff;
+`;
+
 export const StyledTextInput = styled.TextInput`
   margin-top: 8px;
   width: 300px;
   height: 50px;
-  border-radius: 5px;
+  border-radius: 15px;
   padding: 8px;
-  border: 1px solid #303030;
-  border-bottom: 4px solid #303030;
-  border-right: 4px solid #303030;
-  outline: 0;
+  border: 2px solid #303030;
   background-color: #fff;
 `;
 
