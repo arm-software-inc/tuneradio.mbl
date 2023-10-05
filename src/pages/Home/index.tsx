e4f9d7355727d4e7dce894d1821b05424cb9cb8b
murillo.nahas <murillonahas@hotmail.com>
2023-10-05 15:24:15 -0300

    feat: WIP home page

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,5 +1,42 @@
-import { Text } from "react-native";
+import { ContainerView } from "../../assets/styles/ContainerView";
+import { Divisor } from "../../components/Divisor";
+import { Menu } from "../../components/Menu/index";
+
+import {
+  Div,
+  Title,
+  TrendingContainer,
+  Trending,
+  TrendingWrapper,
+  Logo,
+} from "./styles";
 
 export function Home() {
-  return <Text>Home</Text>;
+  return (
+    <ContainerView>
+      <Div>
+        <Title>Trending</Title>
+      </Div>
+
+      <TrendingContainer>
+        <TrendingWrapper>
+          <Trending>
+            <Logo source={require("../../assets/icons/WebRadioIcon.png")} />
+          </Trending>
+        </TrendingWrapper>
+
+        <TrendingWrapper>
+          <Trending>
+            <Logo
+              source={require("../../assets/icons/MetropolitanaIcon.png")}
+            />
+          </Trending>
+        </TrendingWrapper>
+      </TrendingContainer>
+
+      <Divisor backgroundColor="#303030" />
+
+      <Menu />
+    </ContainerView>
+  );
 }
