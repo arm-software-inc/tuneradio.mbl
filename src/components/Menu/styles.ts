71f6030ddcfe80a231536ab46206d090de4142ee
murillo.nahas <murillonahas@hotmail.com>
2023-10-09 15:01:39 -0300

    feat: turning menu component pressable

--- src/components/Menu/styles.ts
+++ src/components/Menu/styles.ts
@@ -3,15 +3,15 @@ import { TouchableOpacity, View } from "react-native";
 import styled from "styled-components/native";
 
 export const MenuWrapper = styled.View`
+  margin: 16px 0px 48px 0px;
   border-radius: 25px;
   border-bottom-width: 3px;
   border-color: #303030;
 `;
 
 export const MenuBar = styled(TouchableOpacity)`
-  margin-top: 34px;
   width: 350px;
-  height: 50px;
+  height: 60px;
   border: 2px solid #303030;
   background-color: #a466ff;
   border-radius: 15px;
